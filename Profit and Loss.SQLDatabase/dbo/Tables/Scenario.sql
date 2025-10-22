dd1df2d54d2641953e6dfeafc189f8a532b05529
christophermichaelbarber-ctrl <christophermichaelbarber@hotmail.com>
2025-10-22 15:20:30 +0100

    Committing 5 items from workspace 8224d2a0-5a21-4b3a-8f7e-47b600566a2c

--- Profit and Loss.SQLDatabase/dbo/Tables/Scenario.sql
+++ Profit and Loss.SQLDatabase/dbo/Tables/Scenario.sql
@@ -1,13 +1,10 @@
 CREATE TABLE [dbo].[Scenario] (
-    [EffectiveDate]      DATE           NULL,
-    [AccountKey]         BIGINT         NULL,
-    [ScenarioKey]        BIGINT         NULL,
-    [LegalEntityKey]     BIGINT         NULL,
-    [Creditlessdebit]    MONEY          NULL,
-    [ScenarioPK]         BIGINT         NULL,
-    [FiscalYear]         BIGINT         NULL,
-    [FiscalPeriod]       NVARCHAR (MAX) NULL,
-    [IncomeStatementKey] BIGINT         NULL
+    [EffectiveDate]   DATE   NULL,
+    [AccountKey]      BIGINT NULL,
+    [ScenarioKey]     BIGINT NULL,
+    [LegalEntityKey]  BIGINT NULL,
+    [Creditlessdebit] BIGINT NULL,
+    [ScenarioPK]      BIGINT NULL
 );
 
 
--- Update Scenario.UserDataFunction/function_app.py
+++ Update Scenario.UserDataFunction/function_app.py
@@ -1,11 +1,35 @@
-import datetime
+
+###############################################################################################################################################################################
+#UDF 1 Commentary
+
 import fabric.functions as fn
-import logging
+##import datetime as datefn
 
 udf = fn.UserDataFunctions()
 
-@udf.function()
-def hello_fabric(name: str) -> str:
-    logging.info('Python UDF trigger function processed a request.')
+@udf.connection(argName="dbo",alias="ProfitandLoss") 
+@udf.function() 
 
-    return f"Welcome to Fabric Functions, {name}, at {datetime.datetime.now()}!"
+# Users will provide these parameters in the PowerBI report
+def write_one_to_sql_db(dbo: fn.FabricSqlConnection, EffectiveDate: str, AccountKey: int, ScenarioKey: int, LegalEntityKey: int, Creditlessdebit: int) -> str: 
+
+    # Establish a connection to the SQL database  
+    connection = dbo.connect() 
+    cursor = connection.cursor() 
+
+    # Insert data into the ProductDescription table  
+    insert_description_query = "INSERT INTO [dbo].[Scenario] (EffectiveDate, AccountKey, ScenarioKey, LegalEntityKey, Creditlessdebit) OUTPUT INSERTED.ScenarioPK VALUES (?, ?, ?, ?, ?)" 
+    
+    
+    
+    
+    cursor.execute(insert_description_query, ( EffectiveDate, AccountKey, ScenarioKey, LegalEntityKey, Creditlessdebit ))
+
+    # Commit the transaction 
+    connection.commit() 
+    cursor.close() 
+    connection.close()  
+
+    return "Scenario was added"
+
+###############################################################################################################################################################################
